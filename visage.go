65589cb751aac0e2bfbf14658e2d3d5bee4041c4
Toby Burress <kurin@google.com>
2017-05-10 05:39:14 +0000

    Add the Directory back.

--- visage.go
+++ visage.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"sort"
+	"strings"
 	"sync"
 )
 
@@ -101,4 +103,35 @@ func (v View) Open(fs, path string, t Token) (io.ReadCloser, error) {
 
 	fsys := v.s.fs[fs]
 	return fsys.Open(path)
+}
+
+// Directory exposes a local directory as a FileSystem.
+type Directory string
+
+func (d Directory) String() string { return string(d) }
+
+func (d Directory) absPath(path string) string {
+	dir := string(d)
+	p := filepath.Join(dir, filepath.Clean(path))
+	if !strings.HasPrefix(p, dir) {
+		p = filepath.Join(dir, p)
+	}
+	return p
+}
+
+func (d Directory) Open(path string) (io.ReadCloser, error) {
+	return os.Open(d.absPath(path))
+}
+
+func (d Directory) Stat(path string) (os.FileInfo, error) {
+	return os.Stat(d.absPath(path))
+}
+
+func (d Directory) ReadDir(path string) ([]os.FileInfo, error) {
+	f, err := os.Open(d.absPath(path))
+	defer f.Close()
+	if err != nil {
+		return nil, err
+	}
+	return f.Readdir(0)
 }
