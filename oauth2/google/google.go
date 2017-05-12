f0ff7c85a9702f8a6a18b63f4fb8b31f3dc72627
Toby Burress <kurin@google.com>
2017-05-12 03:26:56 +0000

    Update and (vastly) simplify the oauth2 API.

--- oauth2/google/google.go
+++ oauth2/google/google.go
@@ -7,13 +7,25 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
+	"net/url"
 
+	"github.com/gorilla/securecookie"
 	"github.com/kurin/visage"
 
 	"golang.org/x/oauth2"
 )
+
+var sc *securecookie.SecureCookie
+
+func init() {
+	hashKey := securecookie.GenerateRandomKey(64)
+	blockKey := securecookie.GenerateRandomKey(32)
+	if hashKey == nil || blockKey == nil {
+		panic("couldn't generate random key")
+	}
+	sc = securecookie.New(hashKey, blockKey)
+}
 
 const googleEndpoint = "https://www.googleapis.com/oauth2/v2/userinfo"
 
@@ -24,20 +36,6 @@ const oauthToken ctxKey = 0
 type access struct {
 	Email    string `json:"email"`
 	Verified bool   `json:"verified_email"`
-}
-
-func WithToken(ctx context.Context, token *oauth2.Token) context.Context {
-	resp, err := http.Get(fmt.Sprintf("%s?access_token=%s", googleEndpoint, token.AccessToken))
-	if err != nil {
-		return ctx
-	}
-	defer resp.Body.Close()
-	dec := json.NewDecoder(resp.Body)
-	acc := &access{}
-	if err := dec.Decode(acc); err != nil {
-		return ctx
-	}
-	return context.WithValue(ctx, oauthToken, acc)
 }
 
 type grant struct {
@@ -54,9 +52,10 @@ func (g *grant) Verify(ctx context.Context) bool {
 	if !ok {
 		return false
 	}
-	return g.allowed[acc.Email] || g.Grant.Verify(ctx)
+	return (g.allowed[acc.Email] && acc.Verified) || g.Grant.Verify(ctx)
 }
 
+// VerifyEmails returns
 func VerifyEmails(g visage.Grant, emails []string) visage.Grant {
 	n := &grant{
 		Grant:   g,
@@ -68,15 +67,14 @@ func VerifyEmails(g visage.Grant, emails []string) visage.Grant {
 	return n
 }
 
-type CookieWrapper interface {
-	Encode(string, interface{}) (string, error)
-	Decode(string, string, interface{}) error
-}
-
-type OAuthHandlers struct {
-	Config        *oauth2.Config
-	Cookie        CookieWrapper
-	InternalError func(http.ResponseWriter, *http.Request, error)
+func RegisterHandlers(path string, config *oauth2.Config) error {
+	http.HandleFunc(path, loginHandler(config))
+	r, err := url.Parse(config.RedirectURL)
+	if err != nil {
+		return err
+	}
+	http.HandleFunc(r.Path, loginReturnHandler(config))
+	return nil
 }
 
 const (
@@ -84,75 +82,89 @@ const (
 	stateCookie = "goog-oauth-state"
 )
 
-func (o *OAuthHandlers) NeedsAuth(h http.HandlerFunc) http.HandlerFunc {
+func Context(ctx context.Context, r *http.Request) context.Context {
+	if _, ok := ctx.Value(oauthToken).(*access); ok {
+		return ctx
+	}
+	c, err := r.Cookie(tokenCookie)
+	if err != nil {
+		return ctx
+	}
+	var tok oauth2.Token
+	if err := sc.Decode(tokenCookie, c.Value, &tok); err != nil {
+		return ctx
+	}
+	resp, err := http.Get(fmt.Sprintf("%s?access_token=%s", googleEndpoint, tok.AccessToken))
+	if err != nil {
+		return ctx
+	}
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	acc := &access{}
+	if err := dec.Decode(acc); err != nil {
+		return ctx
+	}
+	return context.WithValue(ctx, oauthToken, acc)
+}
+
+func loginHandler(config *oauth2.Config) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		c, err := r.Cookie(tokenCookie)
+		b := make([]byte, 32)
+		if _, err := rand.Read(b); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		state := hex.Dump(b)
+		code, err := sc.Encode(stateCookie, state)
 		if err != nil {
-			o.Login(w, r)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		var tok oauth2.Token
-		if err := o.Cookie.Decode(tokenCookie, c.Value, &tok); err != nil {
-			o.Login(w, r)
-			return
-		}
-		req := r.WithContext(WithToken(r.Context(), &tok))
-		h(w, req)
+		http.SetCookie(w, &http.Cookie{
+			Name:  stateCookie,
+			Value: code,
+		})
+		rURL := config.AuthCodeURL(state)
+		http.Redirect(w, r, rURL, http.StatusTemporaryRedirect)
 	}
 }
 
-func (o *OAuthHandlers) Login(w http.ResponseWriter, r *http.Request) {
-	b := make([]byte, 32)
-	if _, err := rand.Read(b); err != nil {
-		o.InternalError(w, r, err)
-		return
+func loginReturnHandler(config *oauth2.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c, err := r.Cookie(stateCookie)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		var state string
+		if err := sc.Decode(stateCookie, c.Value, &state); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		rState := r.FormValue("state")
+		if rState != state {
+			http.Error(w, "oauth2 state mismatch", http.StatusBadRequest)
+			return
+		}
+		code := r.FormValue("code")
+		token, err := config.Exchange(oauth2.NoContext, code)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		enc, err := sc.Encode(tokenCookie, token)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.SetCookie(w, &http.Cookie{
+			Name:  tokenCookie,
+			Value: enc,
+		})
+		rURI := r.FormValue("redirect_uri")
+		if rURI == "" {
+			rURI = "/"
+		}
+		http.Redirect(w, r, rURI, http.StatusTemporaryRedirect)
 	}
-	state := hex.Dump(b)
-	code, err := o.Cookie.Encode(stateCookie, state)
-	if err != nil {
-		o.InternalError(w, r, err)
-		return
-	}
-	http.SetCookie(w, &http.Cookie{
-		Name:  stateCookie,
-		Value: code,
-	})
-	rURL := o.Config.AuthCodeURL(state)
-	http.Redirect(w, r, rURL, http.StatusTemporaryRedirect)
-}
-
-func (o *OAuthHandlers) LoginHandler(w http.ResponseWriter, r *http.Request) {
-	c, err := r.Cookie(stateCookie)
-	if err != nil {
-		o.InternalError(w, r, err)
-		return
-	}
-	var state string
-	if err := o.Cookie.Decode(stateCookie, c.Value, &state); err != nil {
-		o.InternalError(w, r, err)
-		return
-	}
-	rState := r.FormValue("state")
-	if rState != state {
-		log.Print("oauth state mismatch")
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
-	code := r.FormValue("code")
-	token, err := o.Config.Exchange(oauth2.NoContext, code)
-	if err != nil {
-		o.InternalError(w, r, err)
-		return
-	}
-	enc, err := o.Cookie.Encode(tokenCookie, token)
-	if err != nil {
-		o.InternalError(w, r, err)
-		return
-	}
-	http.SetCookie(w, &http.Cookie{
-		Name:  tokenCookie,
-		Value: enc,
-	})
-	rURI := "/" //r.FormValue("redirect_uri")
-	http.Redirect(w, r, rURI, http.StatusTemporaryRedirect)
 }
