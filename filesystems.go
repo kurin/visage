0355a873d2bc72edc77dc94fca961a14fd2e6d29
Toby Burress <kurin@google.com>
2017-05-22 22:51:29 +0000

    Don't export prefab file system types.

--- filesystems.go
+++ filesystems.go
@@ -15,43 +15,127 @@
 package visage
 
 import (
+	"errors"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
-	"strings"
+
+	"golang.org/x/crypto/openpgp"
 )
 
+func NewDirectory(path string) FileSystem { return directory(path) }
+
 // Directory exposes a local directory as a FileSystem.
-type Directory string
+type directory string
 
-func (d Directory) String() string { return string(d) }
+func (d directory) String() string { return string(d) }
 
-func (d Directory) absPath(path string) string {
-	dir := string(d)
-	p := filepath.Join(dir, filepath.Clean(path))
-	if !strings.HasPrefix(p, dir) {
-		p = filepath.Join(dir, p)
-	}
-	return p
+func (d directory) Open(path string) (io.ReadCloser, error) {
+	return os.Open(absPath(string(d), path))
 }
 
-func (d Directory) Open(path string) (io.ReadCloser, error) {
-	return os.Open(d.absPath(path))
+func (d directory) Create(path string) (io.WriteCloser, error) {
+	return os.Create(absPath(string(d), path))
 }
 
-func (d Directory) Create(path string) (io.WriteCloser, error) {
-	return os.Create(d.absPath(path))
+func (d directory) Stat(path string) (os.FileInfo, error) {
+	return os.Stat(absPath(string(d), path))
 }
 
-func (d Directory) Stat(path string) (os.FileInfo, error) {
-	return os.Stat(d.absPath(path))
-}
-
-func (d Directory) ReadDir(path string) ([]os.FileInfo, error) {
-	f, err := os.Open(d.absPath(path))
+func (d directory) ReadDir(path string) ([]os.FileInfo, error) {
+	f, err := os.Open(absPath(string(d), path))
 	defer f.Close()
 	if err != nil {
 		return nil, err
 	}
 	return f.Readdir(0)
+}
+
+func NewEncryptedDirectory(path string, recipients []*openpgp.Entity, signer *openpgp.Entity) FileSystem {
+	return &encryptedDir{
+		root:       path,
+		recipients: recipients,
+		signer:     signer,
+	}
+}
+
+type encryptedDir struct {
+	root       string
+	recipients []*openpgp.Entity
+	signer     *openpgp.Entity
+}
+
+func (e *encryptedDir) String() string {
+	return fmt.Sprintf("%s - encrypted", e.root)
+}
+
+func (e *encryptedDir) Create(path string) (io.WriteCloser, error) {
+	f, err := os.Create(absPath(e.root, path))
+	if err != nil {
+		return nil, err
+	}
+	return openpgp.Encrypt(f, e.recipients, e.signer, &openpgp.FileHints{IsBinary: true}, nil)
+}
+
+type encryptedReader struct {
+	c   io.Closer
+	md  *openpgp.MessageDetails
+	err error
+}
+
+func (er *encryptedReader) Read(p []byte) (int, error) {
+	if er.err != nil {
+		return 0, er.err
+	}
+	i, err := er.md.UnverifiedBody.Read(p)
+	if err == io.EOF && er.md.SignatureError != nil {
+		er.err = er.md.SignatureError
+		return i, er.err
+	}
+	er.err = err
+	return i, err
+}
+
+func (er *encryptedReader) Close() error {
+	return er.c.Close()
+}
+
+func (e *encryptedDir) Open(path string) (io.ReadCloser, error) {
+	f, err := os.Open(absPath(e.root, path))
+	if err != nil {
+		return nil, err
+	}
+	var kr openpgp.EntityList
+	for _, r := range e.recipients {
+		kr = append(kr, r)
+	}
+	kr = append(kr, e.signer)
+	md, err := openpgp.ReadMessage(f, kr, func([]openpgp.Key, bool) ([]byte, error) { return nil, errors.New("not implemented") }, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &encryptedReader{
+		c:  f,
+		md: md,
+	}, nil
+}
+
+func (e *encryptedDir) Stat(path string) (os.FileInfo, error) {
+	return os.Stat(absPath(e.root, path))
+}
+
+func (e *encryptedDir) ReadDir(path string) ([]os.FileInfo, error) {
+	f, err := os.Open(absPath(e.root, path))
+	defer f.Close()
+	if err != nil {
+		return nil, err
+	}
+	return f.Readdir(0)
+}
+
+// absPath returns a path that is guaranteed to be under root.
+func absPath(root, path string) string {
+	path = filepath.Join("/", path)
+	return filepath.Join(root, path)
 }
